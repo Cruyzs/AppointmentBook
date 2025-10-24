df0cc78b21fbe5851b7d2f8257264f54b28434a1
Cruz English <Cruze3@nycstudents.net>
2025-10-24 12:35:55 -0400

    Amended test cases

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,9 @@ public class Main
         for(int i = 44; i < 60; i++) schedule[3][i] = true;
 
         AppointmentBook b = new AppointmentBook(schedule);
-        System.out.println(a.findFreeBlock(4,16));
+        System.out.println(a.findFreeBlock(2,15));
+        System.out.println(a.findFreeBlock(2,9));
+        System.out.println(a.findFreeBlock(2,20));
         int period = 2;
         while(period < 5)
         {
